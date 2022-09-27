ee092e3acbf2d413883efd98bdd1f31ed3b672af
Camilo Villavicencio <laboral@cvillavicencio.com>
2022-09-26 22:19:57 -0300

    vista de listado de links segun tag

--- ll.php
+++ ll.php
@@ -1,18 +1,22 @@
 <?php
-    $u=intval(cleanget('u'));
-    $cat=intval(cleanget('cat'));
-    $sub=intval(cleanget('sub'));
-    $top=intval(cleanget('top'));
-    $pag=intval(cleanget('pag'));
-    $pag = !$pag ? 1 :$pag; 
+$u=intval(cleanget('u'));
+$cat=intval(cleanget('cat'));
+$sub=intval(cleanget('sub'));
+$top=intval(cleanget('top'));
+$pag=intval(cleanget('pag'));
+$tag=cleanget('tag');
+$pag = !$pag ? 1 : $pag; 
 
-    //    $opcion = $u ? array("usuario="," WHERE usrid='$u' ") : false;
+//    $opcion = $u ? array("usuario="," WHERE usrid='$u' ") : false;
 
-    $opciontitulo='';
-    $opcionquery='';
-    $filtro='';
+$opciontitulo='';
+$opcionquery='';
+$filtro='';
 
-    if ( $u || $cat || $sub || $top ) {
+$linksporpag=10;
+$limit = $pag ? "LIMIT $linksporpag OFFSET ".($linksporpag * ($pag-1)) : 'LIMIT '.$linksporpag;
+
+if ( $u || $cat || $sub || $top ) {
 
 	$opcionquery=' WHERE ';
 	if ($u){
@@ -42,24 +46,33 @@
 	    $filtro.=' en tópico '.$rtop['0'];
 	    $opcionquery .="  topicid = '$top' ";
 	}
-	
-    }
 
-    $contenido[]=' ';
-    $estasen='<div class="">Viendo links'.$filtro.'</div>';
+
+}
+
+$sql = "SELECT * FROM Linksinfo $opcionquery ORDER BY creado DESC $limit;";
+
+if($tag) {
+    $sql = "SELECT * FROM Tagslinks INNER JOIN Linksinfo ON Tagslinks.linkId = Linksinfo.id WHERE Tagslinks.tag = '$tag';";
+}
+
+
+$contenido[]=' ';
+$estasen='<div class="">Viendo links'.$filtro.'</div>';
     
-    //listado de links
+//listado de links
 
-    $linksporpag=10;
-    $limit = $pag ? "LIMIT $linksporpag OFFSET ".($linksporpag * ($pag-1)) : 'LIMIT '.$linksporpag;
-    $sql = "SELECT * FROM Linksinfo $opcionquery ORDER BY creado DESC $limit;";
-    //        echo $sql;
-    $result = $conn->query($sql);
+$result = $conn->query($sql) or die(mysqli_error());
 
 
-    $listalinks=' <div class="columns">';
 
-    if ($result->num_rows > 0) {
+
+// ver tags
+
+
+$listalinks=' <div class="columns">';
+
+if ($result->num_rows > 0) {
 	$columncount=1;
 	$listaok=true;
 	while($row = $result->fetch_assoc()) {
@@ -76,45 +89,45 @@
 </div>
 ';
 	    if (is_int($columncount/2)){
-		$listalinks .='</div> <div class="columns">';
-		$listaok=false;
+            $listalinks .='</div> <div class="columns">';
+            $listaok=false;
 	    } else {
-		$listaok=true;
+            $listaok=true;
 	    }
 	    $columncount++;
 	    
 	    /*
-	       SELECT Links.id, Links.info, Links.url, Links.urlextra, Links.creado,
-	       Users.id AS 'usrid', Users.nombre AS 'user',
-	       Topics.id AS 'topicid', Topics.nombre AS 'topic',
-	       Subcategories.id AS 'subcatid', Subcategories.nombre AS 'subcat',
-	       Categories.id AS 'catid', Categories.nombre As 'cat'
-	     */ 
+          SELECT Links.id, Links.info, Links.url, Links.urlextra, Links.creado,
+          Users.id AS 'usrid', Users.nombre AS 'user',
+          Topics.id AS 'topicid', Topics.nombre AS 'topic',
+          Subcategories.id AS 'subcatid', Subcategories.nombre AS 'subcat',
+          Categories.id AS 'catid', Categories.nombre As 'cat'
+        */ 
 
 	    
 	    //    $listalinks .= "  <div class=\"box\">" . $row["url"]. " - " . $row["info"]. " - " . $row["creado"]. "</div>";
 	}
-    }
-    $listalinks.='</div>';
+}
+$listalinks.='</div>';
 
-    $pags = "SELECT COUNT(*) FROM Linksinfo $opcionquery ORDER BY creado DESC";
-    $pagq = $conn->query($pags);
-    $pagl = $pagq->fetch_row();
+$pags = "SELECT COUNT(*) FROM Linksinfo $opcionquery ORDER BY creado DESC";
+$pagq = $conn->query($pags);
+$pagl = $pagq->fetch_row();
 
-    //    echo $pagl[0]; // <-- este es el total de links q existen :-D
-    //paginación
+//    echo $pagl[0]; // <-- este es el total de links q existen :-D
+//paginación
 
-    $totalpags=($pagl[0]/$linksporpag);
-    $paginat=array(''.'');
-    for ($i=0; $i <= $totalpags; $i++) {
+$totalpags=($pagl[0]/$linksporpag);
+$paginat=array(''.'');
+for ($i=0; $i <= $totalpags; $i++) {
 	$o = $i+1;
 	$current = ($o == $pag) ? array(' is-current','aria-current="page"') : array('',''); 
 	$paginat[0] .= '<li><a href="?pag='.$o.'" class="pagination-link'.$current[0].'" aria-label="Goto page 2" '.$current[1].'>'.$o.'</a></li>'.PHP_EOL;	
-    }
+}
 
-    $ant = ($pag > 1) ? array(($pag-1),'',($pag-1)) : array('#','is-disabled','No puedes avanzar más');
-    $sig = ($pag < $totalpags) ? array(($pag+1),'',($pag+1)) : array('#','is-disabled','No puedes retroceder más');
-    $listalinks .= '
+$ant = ($pag > 1) ? array(($pag-1),'',($pag-1)) : array('#','is-disabled','No puedes avanzar más');
+$sig = ($pag < $totalpags) ? array(($pag+1),'',($pag+1)) : array('#','is-disabled','No puedes retroceder más');
+$listalinks .= '
 <nav class="pagination is-small" role="navigation" aria-label="pagination">
   <a href="?pag='.$ant[0].'" class="pagination-previous '.$ant[1].'" title="'.$ant[2].'">Más reciente</a>
   <a href="?pag='.$sig[0].'" class="pagination-next '.$sig[1].'" title="'.$sig[2].'">Más antiguo</a>
@@ -122,6 +135,6 @@
 </nav>
   ';
 
-    $contenido[]=$listalinks;
+$contenido[]=$listalinks;
 ?>
     
