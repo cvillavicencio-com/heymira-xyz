f1089c316330eb843579ad169bd24b9f8fc4b3ba
Camilo Villavicencio <laboral@cvillavicencio.com>
2022-10-07 18:33:06 -0300

    nuevo link: seleccion de categorias mejorada con js

--- nl.php
+++ nl.php
@@ -23,13 +23,12 @@ if ($linkid) { // if está editando link q exista previamente.
         $ts="SELECT * FROM Tagslinks WHERE linkId='$linkid';";
         $tq=$conn->query($ts);
         if ($tq->num_rows > 0) {
-			while($tl = $tq->fetch_assoc()) {
-			    $tags .=$tl['tag'].', ';
-			}
-			$tags=' value="'.substr($tags,0,-2).'" ';
+            while($tl = $tq->fetch_assoc()) {
+                $tags .=$tl['tag'].', ';
+            }
+            $tags=' value="'.substr($tags,0,-2).'" ';
         }
     }
-
 } else {
     $catset = cleanget('catset') ? intval(cleanget('catset')) : '1';
 }
@@ -48,49 +47,51 @@ if ($resultc->num_rows > 0) {
         $colcol++;
 
         $combocat[] = array($rowc['id'],$rowc['nombre']);
-		    
+	
         $cats .='';
-        $cats .='<p id="cat-'.$rowc['id'].'" style="background-color:#444; color:white; z-index:100; position:sticky; top:0px;">'.$rowc['nombre'].'</p>';
+        $cats .='<div class="cscat"><p id="cat-'.$rowc['id'].'" style="background-color:#444; color:white; z-index:100; position:sticky; top:0px;">'.$rowc['nombre'].'</p>';
         $sqls = "SELECT id, nombre FROM Subcategories WHERE categId='".$rowc['id']."';";
         $results = $conn->query($sqls);
         $cats .= '<div class="column">';
         if ($results->num_rows > 0) {
-			while($rows = $results->fetch_assoc()) {
-			    $colorsub = (is_int($colcolsub/2)) ? 'has-background-grey-lighter ': false;
-			    $colcolsub++;
-			    $cats .= '<div class="columns is-1 is-mobile '.$colorsub.'">';
-			    $cats .= '<div class="column '.$colorsub.'is-one-third"><p style="position:sticky; top:0px;"><br><br>'.$rows['nombre'].'</p></div>  <div class="is-divider-vertical" data-content="OR"></div>';
-			    $sqlt = "SELECT id, nombre FROM Topics WHERE subcatId='".$rows['id']."';";
-			    $resultt = $conn->query($sqlt);
-			    $cats .= '<div class="column is-mobile">';
-			    if ($results->num_rows > 0) {
-                    $cats .= '<ul>';
-
+            while($rows = $results->fetch_assoc()) {
+                $colorsub = (is_int($colcolsub/2)) ? 'has-background-grey-lighter ': false;
+                $colcolsub++;
+                $cats .= '<div class="scat block">';
+                $cats .= '<div class="columns is-1 is-mobile '.$colorsub.'">';
+                $cats .= '<div class="column '.$colorsub.'is-one-third"><p style="position:sticky; top:2em;">'.$rows['nombre'].'</div>';
+                $sqlt = "SELECT id, nombre FROM Topics WHERE subcatId='".$rows['id']."';";
+                $resultt = $conn->query($sqlt);
+                $cats .= '<div class="column is-mobile">';
+                if ($results->num_rows > 0) {
+                    $cats .= '<ul">';
                     while($rowt = $resultt->fetch_assoc()) {
                         $elnombre=$rowt['nombre'];
                         if (substr($elnombre,-12) == '[Unassigned]'){
                             $unassig = ' disabled';
                         } else {
                             $unassig = '';
-                        }				    
+                        }
                         $selctd = ($rowt['id'] == @$topicid) ? 'checked' : false;
 
-                        $cats .= '
-	    <li><label class="radio"><input type="radio" name="topic" value="'.$rowt['id'].'" '. $selctd.' '.$unassig.'> '.$rowt['nombre'].'</label></li>';
-                    }
-                    $cats .= '</ul></div>';
-			    } else {
-                    $cats .= 'no hay topicos';
-			    }
-			    $cats .= '</div>';
-			}
 
-			$cats .= '</div>';
+
+                        $cats .= '
+	    <li class="top"><label class="radio"><input type="radio" name="topic" value="'.$rowt['id'].'" '. $selctd.' '.$unassig.' required> '.$rowt['nombre'].'</label></li>';
+                    }
+                    $cats .= '</ul></div></div>';
+                } else {
+                    $cats .= 'no hay topicos';
+                }
+                $cats .= '</div>';
+            }
+
+            $cats .= '</div>';
 
         } else {
-			$cats .= 'no hay subcats';
+            $cats .= 'no hay subcats';
         }
-        $cats .= '';
+        $cats .= '</div>';
     }
 
     $cats .= '';
@@ -99,7 +100,9 @@ if ($resultc->num_rows > 0) {
 }
 
 $comboset='
-	    <div style="display:block-inline;" class="field">
+<div class="columns">
+	    <div class="column">
+        <label class="label is-small">Set</label>
 	    <p class="control has-icons-left">
 	    <span class="select is-small">
 	    <select id="catset" name="catset" onchange="cargasetcat();">';
@@ -120,16 +123,18 @@ $comboset .= '
 	    </span>
 	    </p>
 	    </div>
-	    ';
+
+';
 
 
 
-	    
+
 $combo='
-	    <div class="field">
+	    <div class="column">
+      <label class="label is-small">Categoría</label>
 	    <p class="control has-icons-left">
 	    <span class="select is-small">
-	    <select onchange="jump();" id="combocats">';
+	    <select  onchange="jump();" id="combocats">';
 foreach ($combocat as &$comcat){
     $combo .= '<option value="'.$comcat[0].'">'.$comcat[1] . '</option> - ';
 }
@@ -142,9 +147,51 @@ $combo .= '
 	    </span>
 	    </p>
 	    </div>
-	    ';
-$cats = $comboset .$combo . '<div id="cats">'.$cats.'</div>';
-	    
+</div>
+';
+$buscarapida ='
+
+<script>
+function buscarap(){
+  var br      = document.getElementById(\'br\');
+
+  let cscats = document.getElementsByClassName(\'cscat\');
+  for (let cscat of cscats) {
+    cscat.style.display=\'none\';
+  }
+
+  for (let cscat of cscats) {
+    let scats = cscat.getElementsByClassName(\'scat\')
+    for (let scat of scats) {
+      scat.style.display=\'none\';
+    }
+
+    for (let scat of scats) {
+      let tops = scat.getElementsByClassName(\'top\');
+      for (let top of tops) {
+        top.style.display=\'none\';
+      }
+
+      for (let top of tops) {
+        var tt = top.innerHTML.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        var bb = br.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        if (tt.includes(bb)) {
+          top.style.display=\'block\';
+          scat.style.display=\'block\';
+          cscat.style.display=\'block\';
+
+        } else {
+          top.style.display=\'none\';
+        }
+      }
+    }
+  }
+}
+
+</script>
+<div class="field"><label>Búsqueda rápida</label><input onkeyup="buscarap()" style="width:100%" id="br"></div>';
+$cats = $comboset . $combo . $buscarapida. '<div id="cats">'.$cats.'</div>';
+
 $contenido[] = '
 <form action="?f='.$formedit.'" method="POST">'.@$formidfield.'
 
@@ -152,16 +199,16 @@ $contenido[] = '
   <div class="column">
     <div class="field">
       <label class="label">Título</label>
-      <input name="titulo" class="input" type="text"'.@$titulo.'placeholder="De qué trata este link">
+      <input name="titulo" class="input" type="text"'.@$titulo.'placeholder="De qué trata este link" required>
     </div>
     <div class="field">
       <label class="label">Información sobre el link</label>
-        <textarea class="textarea" rows="10" name="info" placeholder="Por qué compartes este link">'.@$info.'</textarea>
+        <textarea class="textarea" rows="10" name="info" placeholder="Por qué compartes este link" required>'.@$info.'</textarea>
     </div>
 
     <div class="field">
       <label class="label">URL</label>
-      <input name="url" class="input" type="text" placeholder="Dirección del link"'.@$url.'>
+      <input name="url" class="input" type="text" placeholder="Dirección del link"'.@$url.' required>
     </div>
     <div class="field">
       <label class="label is-small">URL extra (opcional)</label>
@@ -173,8 +220,7 @@ $contenido[] = '
   <div class="column">
       <div class="field">
       <label class="label">Tema</label>
-      <label class="label is-small">Tópico</label>
-  	  '.$cats.'
+      '.$cats.'
     </div>
     <div class="field">
       <label class="label is-small"><br>Tags (separados por comas)</label>
@@ -188,111 +234,6 @@ $contenido[] = '
 
 </form>
 
-
-
-
-
-<!--
-	    
-<form action="?f='.$formedit.'" method="POST">'.@$formidfield.'
-
-<div class="columns">
-  <div class="column">
-  </div>
-</div>
-
-  <div class="column">
-    <div class="field">
-      <label class="label">Título</label>
-          <input name="titulo" class="input" type="text"'.@$titulo.'placeholder="De qué trata este link">
-      </div>
-    </div>
-  </div>
-</div>
-
-
-    <div class="field">
-      <label class="label">Información sobre el link</label>
-      <div class="control">
-        <textarea class="textarea" name="info" placeholder="Por qué compartes este link">'.@$info.'</textarea>
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">URL</label>
-      <input name="url" class="input" type="text" placeholder="Dirección del link"'.@$url.'>
-    </div>
-      <div class="field">
-        <label class="label is-small">URL extra (opcional)</label>
-        <input name="urlextra" class="input  is-small" type="text"'.@$urlextra.' placeholder="Respaldo o link relacionado al mismo tema">
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-<hr><hr>aaaa
-<hr>
-<div class="columns">
-  <div class="column">
-    <div class="field">
-      <label class="label">Tema</label>
-	'.$cats.'
-    </div>
-  </div>
-  <div class="column is-half">
-    <div class="field">
-      <label class="label">Tema</label>
-	'.$cats.'
-    </div>
-
-    <div class="field">
-      <label class="label">Título</label>
-      <div class="control">
-          <input name="titulo" class="input" type="text"'.@$titulo.'placeholder="De qué trata este link">
-      </div>
-     </div>
-
-    <div class="field">
-      <label class="label">Información sobre el link</label>
-      <div class="control">
-        <textarea class="textarea" name="info" placeholder="Por qué compartes este link">'.@$info.'</textarea>
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">URL</label>
-
-      <input name="url" class="input" type="text" placeholder="Dirección del link"'.@$url.'>
-    </div>
-  
-    <div class="field">
-      <label class="label is-small">Tags (separados por comas)</label>
-        <div class="control">
-        <input name="tags" class="input is-small" type="text" placeholder="tags, etiquetas, etc" '.@$tags.'>
-    </div>
-  </div>
-
-  <div class="columns is-mobile">
-    <div class="column">
-      <div class="field">
-        <label class="label is-small">URL extra (opcional)</label>
-        <div class="control">
-          <input name="urlextra" class="input  is-small" type="text"'.@$urlextra.' placeholder="Respaldo o link relacionado al mismo tema">
-        </div>
-      </div>
-    </div>
-    <div class="column">
-     <div class="control">
-       <button type="submit" class="button is-primary is-large">'.$actionform.' link</button>
-     </div>
-    </div>
-  </div>
-
-
-
-  </div>
-</div>
--->	 ';
+       ';
 
 ?>
