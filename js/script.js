cf116ac6cd83ced41d6c27f1a5fa305f517e6c1c
Camilo Villavicencio <laboral@cvillavicencio.com>
2022-10-13 21:40:16 -0300

    temas y otros cambios

--- js/script.js
+++ js/script.js
@@ -16,7 +16,52 @@ function cargasetcat(){
     var selset = document.getElementById('catset');
     aa=replaceQueryParam('catset', selset.value, window.location.search);
 
-    if (window.confirm("¿Cambiar set de categorías?\nEl contenido de los campos será reseteado.")) {
+    if (window.confirm("¿Cambiar set de categorías?\nSe perderán todos los cambios que no hayan sido guardados.")) {
 	window.location.replace(aa);
     }
+}
+
+function buscarap(){
+  var br      = document.getElementById('br');
+
+  let cscats = document.getElementsByClassName('cscat');
+  for (let cscat of cscats) {
+    cscat.style.display='none';
+  }
+
+  for (let cscat of cscats) {
+    let scats = cscat.getElementsByClassName('scat')
+    for (let scat of scats) {
+      scat.style.display='none';
+    }
+
+    for (let scat of scats) {
+      let tops = scat.getElementsByClassName('top');
+      for (let top of tops) {
+        top.style.display='none';
+      }
+
+      for (let top of tops) {
+        var tt = top.innerHTML.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        var bb = br.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        if (tt.includes(bb)) {
+          top.style.display='block';
+          scat.style.display='block';
+          cscat.style.display='block';
+
+        } else {
+          top.style.display='none';
+        }
+      }
+    }
+  }
+}
+
+function checkURL (abc) {
+  var string = abc.value;
+  if (!~string.indexOf("http")) {
+    string = "http://" + string;
+  }
+  abc.value = string;
+  return abc
 }
